aeb2b40950b77e4f5a04f3aa0527e41bcac84cce
Michael Gallup <Gallup93@icloud.com>
2020-10-26 17:59:39 -0600

    Add model method to downcase emails before saving a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
