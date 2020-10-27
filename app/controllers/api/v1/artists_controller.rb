31c3b5779b47c91cccdfd5a8655f9ac07b1cf7d8
Michael Gallup <Gallup93@icloud.com>
2020-10-27 17:17:45 -0600

    Add logic for getting and rendering artists

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ArtistsController < ApplicationController
   before_action :authenticate_request!
 
   def index
-    
+    @artists = ArtistCompiler.new(params[:artist]).artists
+    render json: { artists: @artists }, status: :ok
   end
 end
