661fe46a46933899ff250457b51107f42c645187
Michael Gallup <Gallup93@icloud.com>
2020-10-26 18:02:14 -0600

    Stash test. Feel like a dumb and cant quickly figure out testing these requests

---
+++ spec/requests/create_user_spec.rb
@@ -0,0 +1,20 @@
+# require 'rails_helper'
+#
+# RSpec.describe "User creation requests" do
+#   context "a valid request" do
+#     query = {
+#       "user": {
+#         "username": "Hank Hill",
+#         "email": "ProPAIN@aol.com",
+#         "password": "Al@moB33r",
+#         "password_confirmation": "Al@moB33r"
+#       }
+#   }
+#
+#     it "succesfully creates a new user" do
+#       post :create, body: query.to_json, as: :json
+#     end
+#   end
+#
+#
+# end
