2390802554c5a8851078bdf1c1dc80662936e559
Michael Gallup <Gallup93@icloud.com>
2020-10-26 18:01:39 -0600

    Add action for login, Refactor create action to downcase emails

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,23 @@
 require 'bcrypt'
 
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_request!, except: :create
+  before_action :authenticate_request!, except: [:create, :login]
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def login
+    user = User.find_by(email: login_params[:email].to_s.downcase)
+
+    if user&.authenticate(login_params[:password])
+      auth_token = JsonWebToken.encode(user_id: user.id)
+      render json: { auth_token: auth_token }, status: :ok
+    else
+      render json: { error: 'Invalid username/password' }, status: :unauthorized
+    end
+  end
 
   def create
     @user = User.new(user_params)
+    @user.email = @user.email.downcase
 
     if @user.save && @user.authenticate(user_params[:password])
       auth_token = JsonWebToken.encode(user_id: @user.id)
@@ -18,5 +31,8 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+  def login_params
+    params.require(:user).permit(:email, :password)
   end
 end
