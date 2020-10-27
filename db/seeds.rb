81592f18ffd777b5b293add96bc4da71854d26f8
Michael Gallup <Gallup93@icloud.com>
2020-10-27 17:16:22 -0600

    Add artist seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create(followers: 429, name: "Ramakhandra", spotify_id: "1Apw9xiab11PyZLo6YeUoJ", city: "Denver", state: "CO")
+Artist.create(followers: 394, name: "Smirk", spotify_id: "3pwiEWINB62yhDUUODnHLj", city: "Denver", state: "CO")
+Artist.create(followers: 424, name: "American Grandma", spotify_id: "4XTcP25C5ZUWpwf0NYGJnn", city: "Denver", state: "CO")
+Artist.create(followers: 544, name: "Oko Tygra", spotify_id: "0K7C1TRrshf9PGeOmnwtDe", city: "Denver", state: "CO")
+Artist.create(followers: 349, name: "Nightmare Blue", spotify_id: "55YvlSWn4tljj2aEP086Cm", city: "Denver", state: "CO")
+Artist.create(followers: 394, name: "Vic N’ the Narwhals", spotify_id: "0562KfT4fpu6sF69JykFDI", city: "Denver", state: "CO")
+Artist.create(followers: 324, name: "Décollage", spotify_id: "0fcwfASKlWfHUqoeLZBgG3", city: "Denver", state: "CO")
+Artist.create(followers: 329, name: "The Grand Alliance", spotify_id: "6fH3hYOWJdM7BU47CNEuTK", city: "Denver", state: "CO")
+Artist.create(followers: 394, name: "French Kettle Station", spotify_id: "1URhW8Le1fU9cgdhI88JZk", city: "Denver", state: "CO")
+Artist.create(followers: 345, name: "Retrofette", spotify_id: "3WEyHl45TSdOcV4aKSkV6Z", city: "Denver", state: "CO")
+Artist.create(followers: 354, name: "Princess Dewclaw", spotify_id: "2IklmigQ6WNaUiI6NfgcCk", city: "Denver", state: "CO")
+Artist.create(followers: 324, name: "Kiltro", spotify_id: "27CC3tpq7WQR25M03jKTZm", city: "Denver", state: "CO")
+Artist.create(followers: 329, name: "The Still Tide", spotify_id: "7fL0zOWHs9cu8DeyowYIpE", city: "Denver", state: "CO")
+Artist.create(followers: 349, name: "YaSi", spotify_id: "7emRqFqumIU39rRPvK3lbE", city: "Denver", state: "CO")
+Artist.create(followers: 39, name: "Spirettes", spotify_id: "2kOxv0AoEP6Mu6UeJlUORB", city: "Denver", state: "CO")
+Artist.create(followers: 324, name: "Colfax Speed Queen", spotify_id: "3p9nYbFprckRkRxuCFVQcx", city: "Denver", state: "CO")
+Artist.create(followers: 344, name: "The Reminders", spotify_id: "7AGG8NQJMeCQNHK0KHcZyJ", city: "Denver", state: "CO")
+Artist.create(followers: 544, name: "The Velveteers", spotify_id: "79wCmFlJs2qglR5n4UHMoq", city: "Denver", state: "CO")
+Artist.create(followers: 424, name: "Oxeye Daisy", spotify_id: "4PeYW22iXF8Gn8dAMOgBPH", city: "Denver", state: "CO")
+Artist.create(followers: 344, name: "Flaural", spotify_id: "3iqf1QQ0KZwcWutFLcM6cs", city: "Denver", state: "CO")
+Artist.create(followers: 354, name: "Andrew Van Orsdale", spotify_id: "294Yfgkza3W23PuTSLUKuG", city: "Denver")
+Artist.create(followers: 349, name: "Eldren", spotify_id: "5F24VT2NBrbVTIAOmzWBux", city: "Denver", state: "CO")
+Artist.create(followers: 344, name: "iZCALLi", spotify_id: "3Ha0TO43zWQsD2ySFmmp8C", city: "Denver", state: "CO")
+Artist.create(followers: 524, name: "Joel Ansett", spotify_id: "49IjdVEbQcukWy36sdRMzl", city: "Denver", state: "CO")
+Artist.create(followers: 193494, name: "Kid Astronaut", spotify_id: "6WsQsYzrIRYxzIXnHAe20B", city: "Denver", state: "CO")
+Artist.create(followers: 323339, name: "Lena Marie", spotify_id: "4GQzGbGVYfy1rF2JlqQS13", city: "Denver", state: "CO")
+
+# Aurora, CO
+Artist.create(followers: 669, name: "Cottonwood", spotify_id: "4iilkUP52yktrcMlkKNu64", city: "Aurora", state: "CO")
+Artist.create(followers: 59, name: "Kaotic Shock", spotify_id: "6py7mErh7KSU8du33jHeGm", city: "Aurora", state: "CO")
+
+#Fort Collins, CO
+Artist.create(followers: 389, name: "DEBR4H", spotify_id: "1uh5fZXpGgG5FGhmOGCvr6", city: "Fort Collins", state: "CO")
+Artist.create(followers: 119, name: "Glass Cases", spotify_id: "1GVcV5AmbWXwGxW1MRVaTd", city: "Fort Collins", state: "CO")
+
+#Colorado Springs, CO
+Artist.create(followers: 200, name: "Nina de Freitas", spotify_id: "1pzA9y3iQsvZK1hfVWTQRd", city: "Colorado Springs", state: "CO")
+
+#Boulder, CO
+Artist.create(followers: 3493, name: "GODLAZER", spotify_id: "45NBBVb9kWEVWc3AYVNaiy", city: "Boulder", state: "CO")
+Artist.create(followers: 126000, name: "3OH!3", spotify_id: "0FWzNDaEu9jdgcYTbcOa4F", city: "Boulder", state: "CO")
+Artist.create(followers: 99000, name: "Big Gigantic", spotify_id: "7o7mC95EDbJKTcPAAs8C3r", city: "Boulder", state: "CO")
+Artist.create(followers: 349000, name: "Esmé Patterson", spotify_id: "1PoFNQQDFXvOYm6Dx8coAa", city: "Boulder", state: "CO")
+
+#Rockford, IL
+Artist.create(followers: 5000, name: "Taboo Blah Blah", spotify_id: "77WeI8znTJNL9VgXxJVfOO", city: "Rockford", state: "IL")
+Artist.create(followers: 3800, name: "Pink Beam", spotify_id: "1jAlXWRhCvBrpnX8avLVRP", city: "Rockford", state: "IL")
+Artist.create(followers: 666, name: "Purple Hell", spotify_id: "3EusnQEaJ7atdju9f3QoB0", city: "Rockford", state: "IL")
+Artist.create(followers: 16000, name: "Quilt Club", spotify_id: "5PxhTwi9zUw26P4UUBuRs6", city: "Rockford", state: "IL")
+Artist.create(followers: 100000, name: "Name The Moon", spotify_id: "2oWWnEqDToq3n0Hv1zPZQJ", city: "Rockford", state: "IL")
