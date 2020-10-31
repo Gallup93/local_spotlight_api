4d8e0fadc8d63d39e40d141f64133848a1567965
Michael Gallup <Gallup93@icloud.com>
2020-10-31 16:27:33 -0600

    Add 'create' action to ArtistController

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -5,4 +5,21 @@ class Api::V1::ArtistsController < ApplicationController
     @artists = ArtistCompiler.new(params[:artist]).artists
     render json: { artists: @artists }, status: :ok
   end
+
+  def create
+    artist = ArtistStitcher.new(artist_params).stitch
+    if artist.class == Hash
+      render json: artist, status: :unprocessable_entity
+    elsif artist.save
+      render json: { artist: Artist.last.id }, status: :ok
+    else
+      render json: { error: { status: 400, message: "Artist already exists", artist_id: Artist.find_by_spotify_id(artist_params["spotify_id"]).id }}
+    end
+  end
+
+  private
+
+  def artist_params
+    params.permit(:spotify_id, :city, :state)
+  end
 end
