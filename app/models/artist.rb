9b9fc8a14e0e7ff5d33bf04ff2352432bbe4ab03
Michael Gallup <Gallup93@icloud.com>
2020-11-05 15:53:14 -0700

    Refactor Artist model to save city as downcase and state as upcase

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,12 +4,22 @@ class Artist < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :followers, presence: true
+  before_save :downcase_city
+  before_save :upcase_state
 
   def self.index_by_city(city, state)
-    Artist.where(["city = ? and state = ?", "#{city}", "#{state}"])
+    Artist.where(["city = ? and state = ?", "#{city.downcase}", "#{state.upcase}"])
   end
 
   def self.find_by_spotify_id(id)
     Artist.find_by("spotify_id = ?", id)
+  end
+
+  def downcase_city
+    self.city.downcase!
+  end
+
+  def upcase_state
+    self.state.upcase!
   end
 end
